fefe4e10158d3b07a9677f6d8bac1a342ff5fa2c
Ashton Crowder <acrowder2@usf.edu>
2021-02-05 19:54:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Author: Ashton Crowder
+//Date: 2/5/2021
+
+using System;
+
+namespace CrowderDev3Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please Enter Your First & Last Name");
+
+            /* use try catch to validate user input and if the user has bad input the catch will handle the error */
+            try
+            {
+                // This variable will gather data from user input
+                string name = Console.ReadLine();
+
+                // Takes in user input
+                Console.WriteLine("Hello {0}", name);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter your name");
+            }
+        }
+    }
+}
